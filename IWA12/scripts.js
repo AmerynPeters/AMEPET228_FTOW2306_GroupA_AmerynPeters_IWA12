63946d7ec7e738dda64a2d840567e8e2a05d7dc5
AmerynPeters <amerynpeters@gmail.com>
2023-09-22 12:58:18 +0200

    Fixed scripts.js

--- IWA12/scripts.js
+++ IWA12/scripts.js
@@ -1,67 +1,78 @@
-// scripts.js
-
 const STATUS_MAP = {
     shelf: {
-        color: 'green',
+        color: "green",
         canReserve: true,
         canCheckout: true,
         canCheckIn: false,
     },
     reserved: {
-        color: 'blue',
+        color: "blue",
         canReserve: false,
         canCheckout: true,
         canCheckIn: false,
     },
     overdue: {
-        color: 'red',
+        color: "red",
         canReserve: false,
         canCheckout: false,
         canCheckIn: true,
     },
     checkedOut: {
-        color: 'orange',
+        color: "orange",
         canReserve: false,
         canCheckout: false,
         canCheckIn: true,
-    }
+    },
+};
+
+// Edit below line
+
+//making so that the buttons have no color
+
+/* get the button elements */
+const allButtons = document.getElementsByTagName("button");
+/* pull style id from html to change text colour*/
+for (let i = 0; i < allButtons.length; i++) {
+    allButtons[i].style.color = "";
 }
 
-// Edit below line 
-const book1 = {
-    status: document.querySelector('#book1.status'),
-    reserve: document.querySelector('#book1.reserve'),
-    checkout: document.querySelector('#book1.checkout'),
-    checkin: document.querySelector('#book1.checkin')
-    }
-    const book2 = {
-    status: document.querySelector('#book2.status'),
-    reserve: document.querySelector('#book2.reserve'),
-    checkout: document.querySelector('#book2.checkout'),
-    checkin: document.querySelector('#book2.checkin')
-    }
-    const book3 = {
-    status: document.querySelector('#book3.status'),
-    reserve: document.querySelector('#book3.reserve'),
-    checkout: document.querySelector('#book3.checkout'),
-    checkin: document.querySelector('#book3.checkin')
-    }
-    function updateBookStatus(book) {
-    const status = book.status.textContent.trim()
-    const statusInfo = STATUS_MAP[status]
-    book.status.style.color = statusInfo.color
-    book.reserve.disabled = !statusInfo.canReserve
-    book.checkout.disabled = !statusInfo.canCheckout
-    book.checkin.disabled = !statusInfo.canCheckIn
-    }
+//change status to corresp to correct obj.
 
-    updateBookStatus(book1)
-    updateBookStatus(book2)
-    updateBookStatus(book3)
-    const overdue = book1.checkin
-    const reserveCheckout = overdue.style.color = ''
-    const reserved = book2.checkin
-    const reserveCheckin = reserved.style.color =  ''
-    const shelf = book3.checkin
-    const checkIn = shelf.style.color = ''
+/* fetch element to be changed*/
+const fetchStatus = document.getElementsByClassName("status");
 
+const books = document.getElementsByTagName("div");
+
+/* use indexes to change the text colors using the style object 
+(have to use set.attribute method instead of style object
+because style needs to be REPLACED, not changed or updated.
+
+use for loop to loop through all the book divs in html, 
+check status of the spans and change buttons accordingly
+ */
+
+for (let i = 0; i < books.length; i++) {
+    const reserveBtn = books[i].getElementsByClassName("reserve")[0];
+    const checkoutBtn = books[i].getElementsByClassName("checkout")[0];
+    const checkinBtn = books[i].getElementsByClassName("checkin")[0];
+
+    if (fetchStatus[i].innerHTML === "shelf") {
+        fetchStatus[i].setAttribute("style", "color: green");
+        checkinBtn.disabled = true;
+    }
+    if (fetchStatus[i].innerHTML === "reserved") {
+        fetchStatus[i].setAttribute("style", "color: blue");
+        reserveBtn.disabled = true;
+        checkinBtn.disabled = true;
+    }
+    if (fetchStatus[i].innerHTML === "overdue") {
+        fetchStatus[i].setAttribute("style", "color: red");
+        reserveBtn.disabled = true;
+        checkoutBtn.disabled = true;
+    }
+    if (fetchStatus[i].innerHTML === "checkedOut") {
+        fetchStatus[i].setAttribute("style", "color: orange");
+        reserveBtn.disabled = true;
+        checkoutBtn.disabled = true;
+    }
+}
